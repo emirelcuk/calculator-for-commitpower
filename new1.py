0d3c4703d8da1d658f2bd57980ce236a77738c64
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-09-10 15:29:07 +0300

    HVLSN-32 Added simple greeting program. Implement a basic Python script that prompts the user for their name and prints a personalized greeting. Add `greet` function to generate a greeting message. Include `main` function to handle user input.

---
+++ new1.py
@@ -0,0 +1,9 @@
+def greet(name): 
+    """Prints a greeting message with the given name using an f-string for formatting.  
+        Args:
+            name (str): The user's input name.
+
+        Returns:
+            None 
+        """
+    print(f"Hello, {name}!") 
