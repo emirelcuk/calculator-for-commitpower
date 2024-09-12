f49517b0b821d7f9e8e3dcd796a0164cf96fa879
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-09-12 14:45:26 +0300

    HVLSN-29

--- simple.py
+++ simple.py
@@ -1,16 +1,1 @@
-# Simple Python program to print a greeting message
 
-def greet(name):
-    """Prints a greeting message with the given name."""
-    print(f"Hello, {name}!")
-
-# Main function
-def main():
-    # Prompt the user for their name
-    user_name = input("Enter your name: ")
-    # Call the greet function with the user's name
-    greet(user_name)
-
-# Entry point of the program
-if __name__ == "__main__":
-    main()
