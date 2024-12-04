02569e0883a4a25de19a8e34ba097300d71a8860
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-12-04 19:14:34 +0300

    HVLSN-33

--- main.py
+++ main.py
@@ -23,21 +23,7 @@ secim = input("Seçiminizi yapın (1, 2, 3 veya 4): ")
 sayi1 = float(input("Birinci sayıyı girin: "))
 sayi2 = float(input("İkinci sayıyı girin: "))
 
-if secim == "1":
-    sonuc = toplama(sayi1, sayi2)
-    print(f"{sayi1} + {sayi2} = {sonuc}")
-elif secim == "2":
-    sonuc = cikarma(sayi1, sayi2)
-    print(f"{sayi1} - {sayi2} = {sonuc}")
-elif secim == "3":
-    sonuc = carpma(sayi1, sayi2)
-    print(f"{sayi1} * {sayi2} = {sonuc}")
-elif secim == "4":
-    sonuc = bolme(sayi1, sayi2)
-    if isinstance(sonuc, str):
-        print(sonuc)
-    else:
-        print(f"{sayi1} / {sayi2} = {sonuc}")
+
 else:
     print("Geçersiz seçim. Lütfen 1, 2, 3 veya 4 girin.")
 
